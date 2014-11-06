ca8f78ef3cf5602f1689279f48e5297381df6337
Sol <solomon.garger@gmail.com>
2014-11-06 12:45:04 -0600

    changes to edit view

--- app/assets/javascripts/views/edit_project_view.js
+++ app/assets/javascripts/views/edit_project_view.js
@@ -1,9 +1,11 @@
 Showcase.Views.EditProjectView = Backbone.View.extend({
   template: JST["edit"],
   render: function() {
+    $(window).off("scroll");
     var renderedContent;
     renderedContent = this.template({project: this.model});
     this.$el.html(renderedContent);
+    this.$("select").select2();
     return this;
   }
 });
